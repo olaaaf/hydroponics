1966c3d7dfcb336be52e72e2d1c2e949860455cc
olaaaf <o.login@outlook.com>
2021-08-26 15:04:19 +0200

    stg
    sth

--- code/server.py
+++ code/server.py
@@ -15,12 +15,17 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        print(self.path)
         if ".css" in self.path:
             self._set_response('text/css')
         elif ".js" in self.path:
             self._set_response('text/javascript')
         elif ".json" in self.path:
             self._set_response('application/json')
+        elif ".svg" in self.path:
+            self._set_response('image/svg+xml')
+        elif ".ttf" in self.path:
+            self._set_response('font/ttf')
         else:
             self._set_response()
         file = None
